2e98a928e2bd100aef49c783dc30c8feb3c95dad
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-05-31 13:14:32 +0530

    Implement BASE64 sequential

--- Scripts/pastebin.gd
+++ Scripts/pastebin.gd
@@ -2,7 +2,7 @@ extends Node
 class_name PastebinBackend
 
 @export_dir var export_path = "user://"
-enum NewPasteAlgorithms {DECIMAL_SEQUENTIAL,DECIMAL,HEXADECIMAL_SEQUENTIAL,ALPHABET,ALPHABET_SEQUENTIAL,KEYBOARD,KEYBOARD_SEQUENTIAL}
+enum NewPasteAlgorithms {DECIMAL_SEQUENTIAL,DECIMAL,HEXADECIMAL_SEQUENTIAL,ALPHABET,ALPHABET_SEQUENTIAL,BASE64,BASE64_SEQUENTIAL,WORD_BASED}
 @export var paste_name_algorithm:NewPasteAlgorithms
 @export var paste_name_length:int = 5
 func _ready() -> void:
@@ -27,6 +27,11 @@ func create_new_paste(content:String, paste_name:String = ""):
 			while file_exists("%s.html"%get_alphabet_from_number(current_iteration)):
 				current_iteration += 1
 			paste_name = "%s"%get_alphabet_from_number(current_iteration)
+		NewPasteAlgorithms.BASE64_SEQUENTIAL:
+			var current_iteration:int = 0
+			while file_exists("%s.html"%get_base64_from_number(current_iteration)):
+				current_iteration += 1
+			paste_name = "%s"%get_base64_from_number(current_iteration)
 	
 	var file = FileAccess.open(export_path+paste_name+".html", FileAccess.WRITE) #Store file in export path
 	file.store_string(content)
@@ -35,12 +40,6 @@ func file_exists(filename:String) -> bool:
 	return FileAccess.file_exists(export_path+filename)
 
 # Some helper functions
-#func to_hex(number:int) -> String:
-	#var quotient = number/16
-	#var remainder = number%16
-	#var hex_table = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e"]
-	#return "%s%s"%[quotient,hex_table[remainder-1]]
-
 func get_alphabet_from_number(number:int) -> String:
 	var result := ""
 	while number > 0:
@@ -49,3 +48,14 @@ func get_alphabet_from_number(number:int) -> String:
 		result = char(65 + remainumberder) + result  # 65 is ASCII for 'A'
 		number = number / 26
 	return result.to_lower()
+
+func get_base64_from_number(number:int, extended:bool = false):
+	var base64_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_"
+	var result := ""
+	if number == 0:
+		return base64_chars[0]
+	while number > 0:
+		var remainumberder = number % 64
+		result = base64_chars[remainumberder] + result
+		number = number / 64
+	return result
