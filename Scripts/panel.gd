37b76875bebb992955730b6286d1e694ec949f00
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-05-31 22:50:23 +0530

    Change one of the errors.

--- Scripts/panel.gd
+++ Scripts/panel.gd
@@ -12,7 +12,7 @@ func set_main_content(dict:Dictionary):
 		-1: main_content = "No response from server. Is it offline?"
 		0: main_content = "Success!"
 		1: main_content = "No content!"
-		2: main_content = "No filename was specified"
+		2: main_content = "No filename was specified. Could this be a bug?"
 		3: main_content = "Filename was too short. It must be at least 3 characters."
 		4: main_content = "Content was too big! Keep it under 1 Million characters."
 		5: main_content = "Content cannot be empty!"
