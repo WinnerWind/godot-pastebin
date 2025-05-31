843c44df2cacdd9f9e3cad37902ecb3d386ffda4
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-05-31 12:39:50 +0530

    Hexadecimal Sequential code

--- Scripts/pastebin.gd
+++ Scripts/pastebin.gd
@@ -6,19 +6,32 @@ enum NewPasteAlgorithms {DECIMAL_SEQUENTIAL,DECIMAL,HEXADECIMAL_SEQUENTIAL,ALPHA
 @export var paste_name_algorithm:NewPasteAlgorithms
 @export var paste_name_length:int = 5
 func _ready() -> void:
-	create_new_paste("Among us sus!")
+	for i in 16:
+		create_new_paste("Among us sus!")
 	get_tree().root.queue_free()
 
 func create_new_paste(content:String, paste_name:String = ""):
 	match paste_name_algorithm:
 		NewPasteAlgorithms.DECIMAL_SEQUENTIAL:
 			var current_iteration:int = 0
-			while file_exists(str(current_iteration)+".html"):
+			while file_exists("%d.html"%current_iteration):
 				current_iteration += 1
-			paste_name = str(current_iteration)
+			paste_name = "%d"%current_iteration
+		NewPasteAlgorithms.HEXADECIMAL_SEQUENTIAL:
+			var current_iteration:int = 0
+			while file_exists("%x.html"%current_iteration):
+				current_iteration += 1
+			paste_name = "%x"%current_iteration
 	
 	var file = FileAccess.open(export_path+paste_name+".html", FileAccess.WRITE) #Store file in export path
 	file.store_string(content)
 
 func file_exists(filename:String) -> bool:
 	return FileAccess.file_exists(export_path+filename)
+
+# Some helper functions
+#func to_hex(number:int) -> String:
+	#var quotient = number/16
+	#var remainder = number%16
+	#var hex_table = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e"]
+	#return "%s%s"%[quotient,hex_table[remainder-1]]
