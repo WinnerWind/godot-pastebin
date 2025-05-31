48c03d51bda65da59a80564f009a2702706dea27
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-05-31 12:52:13 +0530

    Alphabet based sequential

--- Scripts/pastebin.gd
+++ Scripts/pastebin.gd
@@ -22,6 +22,11 @@ func create_new_paste(content:String, paste_name:String = ""):
 			while file_exists("%x.html"%current_iteration):
 				current_iteration += 1
 			paste_name = "%x"%current_iteration
+		NewPasteAlgorithms.ALPHABET_SEQUENTIAL:
+			var current_iteration:int = 0
+			while file_exists("%s.html"%get_alphabet_from_number(current_iteration)):
+				current_iteration += 1
+			paste_name = "%s"%get_alphabet_from_number(current_iteration)
 	
 	var file = FileAccess.open(export_path+paste_name+".html", FileAccess.WRITE) #Store file in export path
 	file.store_string(content)
@@ -35,3 +40,12 @@ func file_exists(filename:String) -> bool:
 	#var remainder = number%16
 	#var hex_table = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e"]
 	#return "%s%s"%[quotient,hex_table[remainder-1]]
+
+func get_alphabet_from_number(number:int) -> String:
+	var result := ""
+	while number > 0:
+		number -= 1  # Adjust for 1-based inumberdexinumberg
+		var remainumberder := number % 26
+		result = char(65 + remainumberder) + result  # 65 is ASCII for 'A'
+		number = number / 26
+	return result.to_lower()
